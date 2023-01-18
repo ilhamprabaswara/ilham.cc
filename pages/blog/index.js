f70c0a2baf60429e6dc7affb7da58fd4ed2481bd
Ilham <ilhamprabaswara@gmail.com>
2023-01-18 23:19:56 +0700

    Styling and map blog post

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -7,9 +7,23 @@ export default function Blog({ allPostsData }) {
     return (
         <Layout>
             <NavBar />
-            {allPostsData.map(({ date, title, slug }) => (
-                <Link href={`blog/${slug}`}>{title}</Link>
-            ))}
+            <div className="grid gap-10">
+                {allPostsData.map(({ date, img, slug, title }) => (
+                    <>
+                        <article id={slug}>
+                            <img className="rounded mb-[10px]" src={img} />
+                            <h2 className="mb-[5px] text-xl">
+                                <Link href={`blog/${slug}`}>{`${title} ->`}</Link>
+                            </h2>
+                            <div>
+                                <p className="text-[14px] text-neutral-500">
+                                    Updated for 2023. Collection 03 of my Lightroom presets. Each preset has been carefully customised to match the exact style…
+                                </p>
+                            </div>
+                        </article>
+                    </>
+                ))}
+            </div>
         </Layout>
     )
 }
