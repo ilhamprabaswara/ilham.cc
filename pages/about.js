8df9bd31752c833ce6d1c616de4bccf9bd995a60
Ilham <ilhamprabaswara@gmail.com>
2023-01-18 18:03:41 +0700

    Add content

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout"
+import NavBar from "@/components/navbar"
+
+export default function About() {
+    return (
+        <Layout>
+            <NavBar />
+            <h1>About</h1>
+        </Layout>
+    )
+}
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout"
+import NavBar from "@/components/navbar"
+
+export default function Contact() {
+    return (
+        <Layout>
+            <NavBar />
+            <h1>Contact</h1>
+        </Layout>
+    )
+}
