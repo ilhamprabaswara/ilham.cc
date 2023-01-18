25277fd330e371e3f2403512051a90a9cbc1dcfc
Ilham <ilhamprabaswara@gmail.com>
2023-01-18 13:47:15 +0700

    Design and layout

---
+++ components/layout.js
@@ -0,0 +1,3 @@
+export default function Layout({ children }) {
+return <div className="max-w-[1160px] mx-auto px-5">{children}</div>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
