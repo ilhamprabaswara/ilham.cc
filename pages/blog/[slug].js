434c151389902f7b59c3bf4cdfddcb059cb56038
Ilham <ilhamprabaswara@gmail.com>
2023-01-18 23:19:02 +0700

    Add date-fns component

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,16 +1,21 @@
 import Layout from "@/components/layout";
 import NavBar from "@/components/navbar";
 import { getAllPostSlugs, getPostData } from "@/lib/posts";
+import Date from "@/components/date";
 
 export default function Post({ postData }) {
     return (
         <Layout>
             <NavBar />
-            {postData.title}
-            <br />
-            {postData.date}
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <article className="prose">
+                <h1 className="font-medium mb-[10px] text-[22px]">
+                    {postData.title}
+                </h1>
+                <div className="font-medium tracking-[1px] text-[11px] text-slate-400 uppercase">
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 }
