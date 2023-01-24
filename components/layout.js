de3f43926d655edb298b0f1e5225ee8ed5182318
Ilham <ilhamprabaswara@gmail.com>
2023-01-24 23:40:16 +0700

    Change code formatting

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,7 @@
 export default function Layout({ children }) {
-return <div className="max-w-[1160px] mx-auto px-5">{children}</div>;
+    return (
+        <div className="max-w-[1160px] mx-auto mb-5 px-5">
+            {children}
+        </div>
+    );
 }
