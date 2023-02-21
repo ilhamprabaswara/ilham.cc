9270b8619d36e436742b1e0fc3a9e5fb97882e50
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-02-21 23:40:56 +0700

    Debugging

--- pages/spotify.js
+++ pages/spotify.js
@@ -9,6 +9,7 @@ export default function Contact() {
         <Layout>
             <NavBar />
             <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                {console.log(data)}
                 <h1 className="mb-2 text-green-500">{data?.isPlaying ? 'Now Playing' : 'Offline. Last Played'}</h1>
                 <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">{data?.isPlaying ? data.title : 'Stop This Train'}</h5>
                 <p class="font-normal text-gray-700">{data?.isPlaying ? data.artist : 'John Mayer'}</p>
