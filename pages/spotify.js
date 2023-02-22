75af84af0de0225406d3998a9a72f4a7c6b24776
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-02-22 13:21:32 +0700

    Waveform animation

--- pages/spotify.js
+++ pages/spotify.js
@@ -7,7 +7,7 @@ export default function Contact() {
     const { data: nowPlaying } = useSWR('/api/now-playing', fetcher);
     const { data: recentlyPlayed } = useSWR('/api/recently-played', fetcher);
     const checkPlay = () => {
-        if (recentlyPlayed === undefined){
+        if (recentlyPlayed === undefined) {
             return "Undefined"
         } else {
             return recentlyPlayed
@@ -17,7 +17,18 @@ export default function Contact() {
         <Layout>
             <NavBar />
             <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-                <h1 className="mb-2 text-green-500">{nowPlaying?.isPlaying ? 'Now Playing' : 'Offline. Last Played'}</h1>
+                <div className="flex gap-2 mb-3">
+                    {nowPlaying?.isPlaying ?
+                        <div className="flex gap-[5px]">
+                            <div class="wave0" />
+                            <div class="wave1" />
+                            <div class="wave2" />
+                        </div>
+                        :
+                        null
+                    }
+                    <h1 className="text-green-500">{nowPlaying?.isPlaying ? 'Now Playing' : 'Offline. Last Played'}</h1>
+                </div>
                 <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">{nowPlaying?.isPlaying ? nowPlaying.title : checkPlay().title}</h5>
                 <p class="font-normal text-gray-700">{nowPlaying?.isPlaying ? nowPlaying.artist : checkPlay().artist}</p>
             </div>
