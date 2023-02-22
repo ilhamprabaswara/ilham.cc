ae13090a127b0e0d6c3f26a80ffc66b7dcaa0cad
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-02-22 11:44:39 +0700

    Add recently played to Spotify API

--- pages/spotify.js
+++ pages/spotify.js
@@ -4,15 +4,22 @@ import useSWR from "swr"
 
 export default function Contact() {
     const fetcher = (url) => fetch(url).then((r) => r.json());
-    const { data } = useSWR('/api/now-playing', fetcher);
-    // const { data } = useSWR('/api/recently-played', fetcher)
+    const { data: nowPlaying } = useSWR('/api/now-playing', fetcher);
+    const { data: recentlyPlayed } = useSWR('/api/recently-played', fetcher);
+    const checkPlay = () => {
+        if (recentlyPlayed === undefined){
+            return "Undefined"
+        } else {
+            return recentlyPlayed
+        }
+    }
     return (
         <Layout>
             <NavBar />
             <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-                <h1 className="mb-2 text-green-500">{data?.isPlaying ? 'Now Playing' : 'Offline. Last Played'}</h1>
-                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">{data?.isPlaying ? data.title : 'Stop This Train'}</h5>
-                <p class="font-normal text-gray-700">{data?.isPlaying ? data.artist : 'John Mayer'}</p>
+                <h1 className="mb-2 text-green-500">{nowPlaying?.isPlaying ? 'Now Playing' : 'Offline. Last Played'}</h1>
+                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">{nowPlaying?.isPlaying ? nowPlaying.title : checkPlay().title}</h5>
+                <p class="font-normal text-gray-700">{nowPlaying?.isPlaying ? nowPlaying.artist : checkPlay().artist}</p>
             </div>
 
         </Layout>
