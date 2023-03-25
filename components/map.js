4626ea05e3b9123d9cd070cd86bce8fb5a6f99d9
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-26 04:22:37 +0700

    Change duration: 0 to animate: false

--- components/map.js
+++ components/map.js
@@ -68,7 +68,7 @@ const Map = (props) => {
         bounds.extend(coord);
       }
       map.fitBounds(bounds, {
-        duration: 0,
+        animate: false,
         padding: 30,
       });
     });
