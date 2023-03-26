5a581bb935310e87cf729f0e484f11a637636fce
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-27 01:17:45 +0700

    Change styling and layout

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,36 +1,49 @@
-import Layout from "@/components/layout"
-import NavBar from "@/components/navbar"
-import { getSortedPostsData } from "@/lib/posts"
+import Layout from "@/components/layout";
+import NavBar from "@/components/navbar";
+import { getSortedPostsData } from "@/lib/posts";
 import Link from "next/link";
 
 export default function Blog({ allPostsData }) {
-    return (
-        <Layout>
-            <NavBar />
-            <div className="grid gap-10">
-                {allPostsData.map(({ date, excerpt, img, slug, title }) => (
-                    <>
-                        <article id={slug}>
-                            <img className="rounded mb-[10px]" src={img} />
-                            <h2 className="mb-[5px] text-xl">
-                                <Link href={`blog/${slug}`}>{`${title} ->`}</Link>
-                            </h2>
-                            <div>
-                                <p className="text-[14px] text-slate-600">{excerpt}</p>
-                            </div>
-                        </article>
-                    </>
-                ))}
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <NavBar />
+      <div className="grid gap-[60px] md:grid-cols-[1fr_200px]">
+        <main className="grid gap-10 md:grid-cols-2">
+          {allPostsData.map(({ date, excerpt, img, slug, title }) => (
+            <>
+              <article id={slug}>
+                <img className="rounded mb-[10px]" src={img} />
+                <h2 className="mb-[5px] text-slate-700 text-xl font-medium">
+                  <Link href={`blog/${slug}`}>{`${title} ->`}</Link>
+                </h2>
+                <div>
+                  <p className="text-[14px]">{excerpt}</p>
+                </div>
+              </article>
+            </>
+          ))}
+        </main>
+        <aside className="">
+          <figure>
+            <img
+              className="rounded aspect-square object-cover mb-[15px]"
+              src="/ilham-avatar-large.png"
+            ></img>
+            <figcaption className="text-[14px]">
+              Hi! I'm Ilham - A full time nerd. Based in Indonesia
+            </figcaption>
+          </figure>
+        </aside>
+      </div>
+    </Layout>
+  );
 }
 
 export async function getStaticProps() {
-    const allPostsData = getSortedPostsData();
-    return {
-        props: {
-            allPostsData
-        }
-    }
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
