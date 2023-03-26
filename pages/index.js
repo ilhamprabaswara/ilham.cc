76427a6cb219f4a13ac1f3fa09c412a2eac4a23b
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-27 01:15:17 +0700

    Remove now playing from landing page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Layout from '@/components/layout'
 import NavBar from '@/components/navbar'
-import NowPlaying from '@/components/nowPlaying'
 import Link from 'next/link'
 import { getSortedPostsData } from "@/lib/posts"
 
@@ -19,8 +18,8 @@ export default function Home({ allPostsData }) {
         <main>
           <div className='mb-[50px]'>
             <img className='mb-[25px] rounded md:w-[500px]' src='/ilham-avatar-large.png' />
-            <p className='font-semibold text-[22px]  text-slate-900 leading-normal'>Hi! I’m Ilham — Full time nerd.</p>
-            <p className='text-[16px] max-w-[800px] text-slate-500 leading-normal'>A front-end enthusiast from Indonesia. I'm interested in React, Node, visual design, bikes, photography, and music.</p>
+            <p className='font-semibold text-[22px]  text-slate-700 leading-normal'>Hi! I’m Ilham — Full time nerd.</p>
+            <p className='text-[16px] max-w-[800px] leading-normal'>A front-end enthusiast from Indonesia. I'm interested in React, Node, visual design, bikes, photography, and music.</p>
           </div>
           <div className='mb-5 uppercase text-[11px] tracking-[2px] font-semibold'>Blog</div>
           <div className="grid gap-10 mb-10 md:grid-cols-3">
@@ -28,17 +27,16 @@ export default function Home({ allPostsData }) {
               <>
                 <article id={slug}>
                   <img className="rounded mb-[10px]" src={img} />
-                  <h2 className="mb-[5px] text-xl">
+                  <h2 className="mb-[5px] text-xl text-slate-700 font-medium">
                     <Link href={`blog/${slug}`}>{`${title} ->`}</Link>
                   </h2>
                   <div>
-                    <p className="text-[14px] text-slate-600">{excerpt}</p>
+                    <p className="text-[14px]">{excerpt}</p>
                   </div>
                 </article>
               </>
             ))}
           </div>
-          <NowPlaying />
         </main>
       </Layout>
     </>
