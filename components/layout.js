9bd5c03b0f6682e1999a73de55a79a30a2fb9db0
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-27 01:14:01 +0700

    Change font color

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,6 @@
 export default function Layout({ children }) {
     return (
-        <div className="max-w-[1160px] mx-auto mb-5 px-5">
+        <div className="max-w-[1160px] mx-auto mb-5 px-5 text-slate-500">
             {children}
         </div>
     );
