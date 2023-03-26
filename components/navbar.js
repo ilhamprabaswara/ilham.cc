7e0fc254ea1767a7c44b87cc788dd5a782a4dd2a
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-27 01:14:44 +0700

    Change navbar content

--- components/navbar.js
+++ components/navbar.js
@@ -8,12 +8,12 @@ export default function NavBar() {
     }
     return (
         <header>
-            <nav className="border-b border-gray-200 mb-5 py-2.5">
+            <nav className="border-b border-gray-200 mb-5 py-2.5 text-slate-700">
                 <div className="container flex flex-wrap items-center justify-between mx-auto">
                     <Link href="/" className="flex items-center">
                         <span className="font-Raleway self-center text-xl font-semibold whitespace-nowrap">ilham</span>
                     </Link>
-                    <button onClick={handleHidden} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+                    <button onClick={handleHidden} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-slate-700 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                     </button>
@@ -22,10 +22,10 @@ export default function NavBar() {
                             {[
                                 ['Blog', '/blog'],
                                 ['About', '/about'],
-                                ['Contact', '/contact'],
+                                ['Work', '/work'],
                             ].map(([title, url]) => (
                                 <li key={title}>
-                                    <Link className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" href={url}>{title}</Link>
+                                    <Link className="block font-semibold py-2 pl-3 pr-4 text-slate-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0" href={url}>{title}</Link>
                                 </li>
                             ))}
                         </ul>
