e72f2128248491e3e2d4fb1a98470a2aa6f70141
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-27 01:14:44 +0700

    Change navbar content

--- pages/work.js
+++ pages/work.js
@@ -1,11 +1,11 @@
 import Layout from "@/components/layout"
 import NavBar from "@/components/navbar"
 
-export default function Contact() {
+export default function Work() {
     return (
         <Layout>
             <NavBar />
-            <h1>Contact</h1>
+            <h1>Work</h1>
         </Layout>
     )
 }
