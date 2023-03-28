acaf60b9a67b1fec937ddccd417fb0e7090a1eb9
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-28 17:25:34 +0700

    Add head tag to layour component

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,15 @@
+import Head from "next/head";
+
 export default function Layout({ children }) {
-    return (
-        <div className="max-w-[1160px] mx-auto mb-5 px-5 text-slate-500">
-            {children}
-        </div>
-    );
+  return (
+    <div className="max-w-[1160px] mx-auto mb-5 px-5 text-slate-500">
+      <Head>
+        <title>Ilham Prabaswara | Web Developer</title>
+        <meta name="description" content="Made with love by Ilham" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/img/favicon.ico" />
+      </Head>
+      {children}
+    </div>
+  );
 }
