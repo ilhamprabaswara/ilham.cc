6198b64e94ab0534590d3c34ee52687e70bec3c6
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-04-05 00:47:17 +0700

    Add footer and navbar to layout component

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,21 @@
 import Head from "next/head";
+import Footer from "./footer";
+import NavBar from "./navbar";
 
 export default function Layout({ children }) {
   return (
-    <div className="max-w-[1160px] mx-auto mb-5 px-5 text-slate-500">
+    <>
       <Head>
         <title>Ilham Prabaswara | Web Developer</title>
         <meta name="description" content="Made with love by Ilham" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/img/favicon.ico" />
       </Head>
-      {children}
-    </div>
+      <div className="box-border mx-auto mb-[50px] px-4 text-[#45464F]">
+        <NavBar />
+        {children}
+      </div>
+      <Footer />
+    </>
   );
 }
