57862578372d2d718c9dd5f3a1b52c014a0783dd
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-04-04 22:37:50 +0700

    Add blog list component

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,45 @@
-import Head from 'next/head'
-import Layout from '@/components/layout'
-import NavBar from '@/components/navbar'
-import Link from 'next/link'
-import { getSortedPostsData } from "@/lib/posts"
+import Layout from "@/components/layout";
+import NavBar from "@/components/navbar";
+import BlogList from "../components/blogList";
+import { getSortedPostsData } from "@/lib/posts";
 
 export default function Home({ allPostsData }) {
   return (
     <>
       <Layout>
-        <NavBar />
         <main>
-          <div className='mb-[50px]'>
-            <img className='mb-[25px] rounded md:w-[500px]' src='/ilham-avatar-large.png' />
-            <p className='font-semibold text-[22px]  text-slate-700 leading-normal'>Hi! I’m Ilham — Full time nerd.</p>
-            <p className='text-[16px] max-w-[800px] leading-normal'>A front-end enthusiast from Indonesia. I'm interested in React, Node, visual design, bikes, photography, and music.</p>
+          <div className="mb-[50px]">
+            <img
+              className="h-[215px] mb-2.5 object-cover rounded-lg md:w-[500px]"
+              src="/ilham-avatar-large.png"
+            />
+            <p className="font-semibold text-xl text-[#1B1B1F] leading-[1.4em]">
+              Hi! I’m Ilham — Full time nerd.
+            </p>
+            <p className="text-[14px] max-w-[800px] leading-[1.6em]">
+              A front-end enthusiast from Indonesia. I'm interested in React,
+              Node, visual design, bikes, photography, and music.
+            </p>
           </div>
-          <div className='mb-5 uppercase text-[11px] tracking-[2px] font-semibold'>Blog</div>
-          <div className="grid gap-10 mb-10 md:grid-cols-3">
-            {allPostsData.map(({ date, excerpt, img, slug, title }) => (
-              <>
-                <article id={slug}>
-                  <img className="rounded mb-[10px]" src={img} />
-                  <h2 className="mb-[5px] text-xl text-slate-700 font-medium">
-                    <Link href={`blog/${slug}`}>{`${title} ->`}</Link>
-                  </h2>
-                  <div>
-                    <p className="text-[14px]">{excerpt}</p>
-                  </div>
-                </article>
-              </>
-            ))}
+          <div className="blog">
+            <div className="mb-5 uppercase text-[11px] tracking-[2px] font-semibold">
+              Blog
+            </div>
+            <div className="grid gap-10 mb-10 md:grid-cols-3">
+              <BlogList {...allPostsData} />
+            </div>
           </div>
         </main>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
