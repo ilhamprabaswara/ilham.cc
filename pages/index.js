31ff16029e8be2588339c622e0f6f6efc081174e
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-05-04 11:53:17 +0700

    Change landing page design

--- pages/index.js
+++ pages/index.js
@@ -10,24 +10,22 @@ export default function Home({ allPostsData }) {
     <>
       <Layout>
         <main>
-          <div className="mb-[50px]">
+          <div className="mb-[50px] md:w-[648px]">
             <img
               className="h-[215px] mb-2.5 object-cover rounded-lg md:h-[325px] md:w-[552px]"
               src="/ilham-avatar-large.png"
             />
-            <p className="font-semibold text-xl text-[#1B1B1F] leading-[1.4em] md:text-2xl md:mb-[5px]">
-              Hi! I’m Ilham — Full time nerd.
-            </p>
-            <p className="text-[14px] max-w-[800px] leading-[1.6em] md:text-base">
-              A front-end enthusiast from Indonesia. Interested in React,
-              Node, visual design, bikes, photography, and music.
+            <p className="font-semibold text-xl leading-[1.6em] text-[#1B1B1F] md:text-2xl md:leading-[1.6em]">Hi! I’m Ilham — Full time nerd.</p>
+            <p className="text-sm leading-[1.6em] text-[#45464F] md:text-xl md:leading-[1.6em]">
+              A front-end enthusiast from Indonesia. Interested in React, Node,
+              visual design, bikes, photography, and music.
             </p>
           </div>
           <div className="blog">
-            <div className="mb-5 uppercase text-[11px] tracking-[2px] font-semibold">
+            <div className="font-semibold text-[11px] leading-[1.6em] tracking-[2px] uppercase mb-5">
               Blog
             </div>
-            <div className="grid gap-6 mb-10 md:grid-cols-3">
+            <div className="grid gap-10 mb-10 md:grid-cols-3">
               <BlogList {...allPostsData} />
             </div>
             <Link href={`/blog`}>
