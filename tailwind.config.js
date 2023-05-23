f98ac480f0b97aa9a3cf772c2b39979956f1f0a5
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-05-23 23:49:15 +0700

    Add design system to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      secondary60: "#8C90A6",
+    },
     extend: {
       fontFamily: {
         Inter: ["Inter", "sans-serif"],
