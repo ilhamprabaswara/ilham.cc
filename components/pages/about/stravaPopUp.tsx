f4ee4f40f810c338b7b322ed5faa50e8be90f7f2
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-08-14 22:13:04 +0700

    feat(stravaPopUp): Add next and prev button functionality

--- components/pages/about/stravaPopUp.tsx
+++ components/pages/about/stravaPopUp.tsx
@@ -1,8 +1,9 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
 export default function StravaPopUp() {
+  const [currentIndex, setCurrentIndex] = useState(null);
   const fetcher = (url) => fetch(url).then((r) => r.json());
   const { data, error, isLoading } = useSWR("/api/workout-activity", fetcher);
 
@@ -12,20 +13,46 @@ export default function StravaPopUp() {
     const { pathname } = router;
     router.push({ pathname }, undefined, { scroll: false });
   };
-  data.activity = data.activities.filter((i) => {
-    if (i.id === Number(card)) {
-      return i;
+  console.log(data)
+  useEffect(() => {
+    if (data && card) {
+      const index = data.activities.findIndex(
+        (activity) => activity.id === Number(card)
+      );
+      setCurrentIndex(index !== -1 ? index : null);
     }
-  });
-  console.log(data.activity);
+  }, [data, card]);
+  useEffect(() => {
+    if (currentIndex !== null) {
+      router.push(
+        {
+          query: { ...router.query, id: currentActivity.id },
+        },
+        undefined,
+        { scroll: false }
+      );
+    }
+  }, [currentIndex]);
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % data.activities.length);
+  };
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + data.activities.length) % data.activities.length
+    );
+  };
+  const currentActivity = data.activities[currentIndex];
   return (
     <>
-      {card && (
+      {card && currentActivity && (
         <div className="fixed flex items-center justify-center top-0 left-0 w-screen h-screen bg-secondary60 bg-opacity-50 z-[9999] overflow-hidden">
-          <div className="relative bg-[#FFFFFF] h-[200px] w-[200px]">
+          <div className="relative transition-all duration-1000 bg-[#FFFFFF] h-[200px] w-[200px]">
             <button onClick={removeQuery}>Close</button>
-            <h1 className=" text-[#212121]">{data.activity[0].name}</h1>
-            <h1 className=" text-[#212121]">{data.activity[0].distance}</h1>
+            <h1 className=" text-[#212121]">{currentActivity.name}</h1>
+            <h1 className=" text-[#212121]">{currentActivity.distance}</h1>
+            <button onClick={prevSlide}>Prev</button>
+            <button onClick={nextSlide}>Next</button>
           </div>
         </div>
       )}
