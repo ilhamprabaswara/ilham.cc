b7b94b4dabd3a4c56c212bdd61dae07269104597
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-09-26 21:02:35 +0700

    test: force majeure commit

--- components/pages/home/heroSection.tsx
+++ components/pages/home/heroSection.tsx
@@ -1,21 +1,11 @@
-import Image from "next/image";
-
 const HeroSection = () => {
   return (
     <section className="hero">
-      <div className="mb-[50px]">
-        <Image
-          alt="avatar"
-          className="h-[215px] w-full mb-2.5 object-cover rounded-lg md:h-[325px] md:w-[552px]"
-          src="/ilham-avatar-large.webp"
-          priority={true}
-          width={552}
-          height={325}
-        />
-        <p className="font-semibold text-xl leading-[1.6em] text-[#1B1B1F] md:text-2xl md:leading-[1.6em]">
+      <div className="py-20">
+        <p className="text-title-lg font-semibold text-sys-light-on-background">
           Hi! I’m Ilham — Full time nerd.
         </p>
-        <p className="text-sm leading-[1.6em] text-[#45464F] md:text-xl md:leading-[1.6em]">
+        <p className="text-body-lg text-sys-light-on-surface-variant">
           A front-end enthusiast from Indonesia. Interested in React, Node,
           visual design, bikes, photography, and music.
         </p>
