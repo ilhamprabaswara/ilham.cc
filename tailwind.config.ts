689d6475e96e4048c16583e051f59e39501ddd9c
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-10-07 09:11:17 +0700

    feat(styling): add design system to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,28 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        sys: {
+          light: {
+            background: '#FEFBFF',
+            'on-background': '#1B1B1F',
+            'on-surface': '#1B1B1F',
+            'on-surface-variant': '#45464F',
+            outline: '#767680',
+            primary: '#004EE8',
+            'surface-container': '#EFEDF1',
+          },
+          dark: {
+            background: '#1B1B1F',
+            'on-background': '#E4E1E6',
+            'on-surface': '#C8C6CA',
+            'on-surface-variant': '#C6C5D0',
+            outline: '#90909A',
+            primary: '#B6C4FF',
+            'surface-container': '#1F1F23',
+          },
+        },
+      },
     },
   },
   plugins: [],
