07b11898b2d82e2f9ea380ac15ebb0fce52f046d
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-10-07 13:46:27 +0700

    refactor: apply prettier sort

--- src/components/landing/headline.tsx
+++ src/components/landing/headline.tsx
@@ -1,7 +1,7 @@
 export const Headline = () => {
   return (
-    <div className="max-w-lg px-4 pt-20 pb-[90px] md:px-6 xl:px-0">
-      <h1 className="font-bold text-title-lg mb-1">
+    <div className="max-w-lg px-4 pb-[90px] pt-20 md:px-6 xl:px-0">
+      <h1 className="mb-1 text-title-lg font-bold">
         I’m Ilham — full time nerd.
       </h1>
       <p className="text-body-lg text-sys-light-on-surface-variant dark:text-sys-dark-on-surface-variant">
