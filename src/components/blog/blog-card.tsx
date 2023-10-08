56ebf6202f8d742741b1236e28119b11b896f65b
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-10-08 09:48:06 +0700

    fix(styles): change intersection threshold

--- src/components/blog/blog-card.tsx
+++ src/components/blog/blog-card.tsx
@@ -28,7 +28,7 @@ export const BlogCard = ({
           }
         })
       },
-      { threshold: 0.2 }
+      { threshold: 0 }
     )
 
     if (cardRef.current) {
@@ -40,27 +40,28 @@ export const BlogCard = ({
     }
   }, [])
   return (
-    <Link
-      ref={cardRef}
-      className={`blog-card group flex h-full flex-col overflow-hidden rounded-2xl border border-sys-light-outline transition-all duration-[400ms] hover:bg-[#6750A414]`}
-      href={'#'}
-    >
-      <div className="h-[180px] md:h-[150px] xl:h-[200px]">
-        <div className="h-full overflow-clip">
-          <Image
-            className="h-full object-cover transition-all duration-[400ms] group-hover:scale-[1.03]"
-            src={imageSrc}
-            alt="card-image"
-          />
+    <li ref={cardRef} className="blog-card h-full">
+      <Link
+        className={`group flex h-full flex-col overflow-hidden rounded-2xl border border-sys-light-outline transition-all duration-[400ms] hover:bg-[#6750A414]`}
+        href={'#'}
+      >
+        <div className="h-[180px] md:h-[150px] xl:h-[200px]">
+          <div className="h-full overflow-clip">
+            <Image
+              className="h-full object-cover transition-all duration-[400ms] group-hover:scale-[1.03]"
+              src={imageSrc}
+              alt="card-image"
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex h-full flex-col justify-between p-6">
-        <div className="font-bold">
-          <div className="mb-2 text-label-base uppercase">{category}</div>
-          <div className="text-title-lg">{title}</div>
+        <div className="flex h-full flex-col justify-between p-6">
+          <div className="font-bold">
+            <div className="mb-2 text-label-base uppercase">{category}</div>
+            <div className="text-title-lg">{title}</div>
+          </div>
+          <div className="mt-2 font-semibold">{date}</div>
         </div>
-        <div className="mt-2 font-semibold">{date}</div>
-      </div>
-    </Link>
+      </Link>
+    </li>
   )
 }
