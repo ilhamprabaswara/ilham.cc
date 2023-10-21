ac746eeb4286cf59e148bc040e04aa6ab586204e
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-10-21 22:40:32 +0700

    feat(test): Change unit test coverage

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,7 +5,7 @@ const createJestConfig = nextJest({
   dir: './',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
-  }
+  },
 })
 
 // Add any custom config to be passed to Jest
@@ -14,6 +14,7 @@ const config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverageFrom: ['<rootDir>/src/**'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
