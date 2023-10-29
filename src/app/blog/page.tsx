431c013ea4d65e7d126243d9b5c8bbd28add08c9
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-10-29 13:50:08 +0700

    feat(types): Add types for blog list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ import { BlogList } from '@/components/blog/blog-list'
 import { getSortedPostsData } from '@/lib/posts'
 
 export default function Blog() {
-  const posts = getSortedPostsData()
+  const posts = getSortedPostsData() as BlogPostInterface[]
   return (
     <>
       <div className="mx-auto mt-[30px] grid max-w-[1128px] gap-6">
--- src/components/blog/blog-list.tsx
+++ src/components/blog/blog-list.tsx
@@ -1,21 +1,23 @@
 import { BlogCard } from './blog-card'
 
-export const BlogList = ({ data }: any) => {
+export const BlogList = ({ data }: { data: BlogPostInterface[] }) => {
   return (
     <ul className="grid gap-6 md:grid-cols-3">
-      {data?.map(({ slug, category, title, date, image }: BlogDataInterface) => {
-        return (
-          <li key={slug} className="h-full">
-            <BlogCard
-              category={category}
-              title={title}
-              date={date}
-              image={image}
-              slug={slug}
-            />
-          </li>
-        )
-      })}
+      {data?.map(
+        ({ slug, category, title, date, image }: BlogPostInterface) => {
+          return (
+            <li key={slug} className="h-full">
+              <BlogCard
+                category={category}
+                title={title}
+                date={date}
+                image={image}
+                slug={slug}
+              />
+            </li>
+          )
+        }
+      )}
     </ul>
   )
 }
